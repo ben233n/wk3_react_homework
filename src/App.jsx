01d249ab36706e01b50cdd7f59c319cb83834dc0
http1224com <ben233n@gmail.com>
2025-03-11 18:29:27 +0800

    head

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import Head from './components/Head/head'
+import Head from './components/Head/Head'
 import Big from './components/Big/Big'
 import New from './components/New/New'
 import Case from './components/Case/Case'
